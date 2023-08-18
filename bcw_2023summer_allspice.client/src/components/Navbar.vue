0fd7eaeab4f1b88f4e6e4d41458a24abcf058c32
Arrington Mata <mataarrington@gmail.com>
2023-08-17 20:05:16 -0600

    Started Making Navbar 🏗

--- bcw_2023summer_allspice.client/src/components/Navbar.vue
+++ bcw_2023summer_allspice.client/src/components/Navbar.vue
@@ -1,24 +1,16 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+  <nav class="navbar navbar-expand-md">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarToggler">
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search Recipes...">
+          <button class="btn btn-outline-primary" type="submit"><i class="mdi mdi-magnify"></i></button>
+        </form>
+        <Login />
       </div>
-    </router-link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto">
-        <li>
-          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
-            About
-          </router-link>
-        </li>
-      </ul>
-      <!-- LOGIN COMPONENT HERE -->
-      <Login />
     </div>
   </nav>
 </template>
@@ -34,23 +26,5 @@ export default {
 </script>
 
 <style scoped>
-a:hover {
-  text-decoration: none;
-}
 
-.nav-link {
-  text-transform: uppercase;
-}
-
-.navbar-nav .router-link-exact-active {
-  border-bottom: 2px solid var(--bs-success);
-  border-bottom-left-radius: 0;
-  border-bottom-right-radius: 0;
-}
-
-@media screen and (min-width: 768px) {
-  nav {
-    height: 64px;
-  }
-}
 </style>
